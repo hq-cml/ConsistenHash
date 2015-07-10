dd9b994862aeaa9a4379ee3770a6df6b09acf68e
hq <hq_cml@163.com>
2015-07-10 18:42:48 +0800

    my hash

--- ConsistentHash.php
+++ ConsistentHash.php
@@ -1,5 +1,12 @@
 <?php
     class ConsistentHash
     {
+        private $slots_arr = array();
+        private $is_sorted = false;
+        
+        function my_hash($key)
+        {
+            return crc32($key);
+        }
     }
     
