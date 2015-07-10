1ca7f3bcdf25ec9ae2e06958ec1cf0dfba889aca
hq <hq_cml@163.com>
2015-07-10 18:43:08 +0800

    add slot

--- ConsistentHash.php
+++ ConsistentHash.php
@@ -8,5 +8,17 @@
         {
             return crc32($key);
         }
+        function hash_add_slot($element)
+        {
+            $hash_value = $this->my_hash($element);
+            echo $hash_value."\n";
+            if(!isset($this->slots_arr[$hash_value]))
+            {
+                $this->slots_arr[$hash_value] = $element;
+            }
+            
+            $this->is_sorted = false;
+            return true;
+        }
     }
     
